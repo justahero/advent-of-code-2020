aa6f80d3c76a7fdc8411b15791dcc59f9dda00b4
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-25 13:01:40 +0100

    Implement running version to find pair of numbers
    This is a basic implementation that finds a pair of numbers that sum to
    2020. It then prints out the product of both numbers.

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,35 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
 fn main() {
-    println!("Hello");
+    let numbers = read_numbers("./input.txt").expect("Failed to open file");
+    let mut sum = 0;
+
+    for i in 0..numbers.len() - 1 {
+        let left = numbers[i];
+        for j in i..numbers.len() {
+            let right = numbers[j];
+            if left + right == 2020 {
+                println!("{} + {} = 2020", left, right);
+                sum = left * right;
+                break;
+            }
+        }
+    }
+
+    println!("SUM: {}", sum);
+}
+
+fn read_numbers(path: &str) -> Result<Vec<i32>, std::io::Error> {
+    let mut numbers = Vec::new();
+
+    let lines = BufReader::new(File::open(path)?).lines();
+    for line in lines {
+        if let Ok(line) = line {
+            if let Ok(number) = line.trim().parse::<i32>() {
+                numbers.push(number);
+            }
+        }
+    }
+
+    Ok(numbers)
 }

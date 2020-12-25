131b45892a12ccc89d8e9b170f9d87bfc262e29c
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-25 18:43:31 +0100

    Implement validation of passports

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,19 +1,36 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::{HashMap, HashSet}};
 
 /// Returns true if the given passport is valid
 /// It is valid when the following requirements are met
 ///
-/// * 
-fn valid_passport(_passport: &[&str]) -> bool {
-    true
+/// * expected keys are: byr, iyr, eyr, hgt, hcl, ecl, pid, cid
+/// * only missing field cid is a North Pole Credential, also valid
+/// * if cid & another field are missing, invalid!
+fn valid_passport(passport: &HashMap<&str, &str>) -> bool {
+    let expected: HashSet<_> = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"].into_iter().collect();
+    let keys: HashSet<_> = passport.keys().cloned().collect();
+
+    let result = expected.difference(&keys).collect::<Vec<_>>();
+
+    result.is_empty() || result.cmp(&vec![&"cid"]) == Ordering::Equal
 }
 
 fn main() {
-    // Vec of String Iterators
+    // Read, parse and generate passport as a list of key / value pairs
     let passports = include_str!("passports.txt")
         .split("\n\n")
         .map(|line| line.split_ascii_whitespace().collect::<Vec<_>>())
         .map(|passport| -> HashMap::<&str, &str> {
+            /*
+            passport
+                .into_iter()
+                .map(|pair| pair.split('.').collect::<Vec<_>>())
+                .filter(|pair| pair.len() == 2)
+                .fold(HashMap::new(), |mut result, pair| {
+                    result.insert(pair[0], pair[1]);
+                    result
+                })
+            */
             //  valid_passport(passport)
             // HashMap::new()
             let mut map = HashMap::new();
@@ -27,13 +44,11 @@ fn main() {
         })
         .collect::<Vec<HashMap<&str, &str>>>();
 
-    dbg!(&passports);
+    // check all passports
+    let passports = passports
+        .into_iter()
+        .filter(|passport| valid_passport(passport))
+        .collect::<Vec<_>>();
 
-    /*
-    let mut passports = Vec::new();
-    for (key, group) in &lines.group_by(|key| key.trim().is_empty()) {
-        let x = group.[true];
-        passports.push();
-    }
-    */
+    dbg!(&passports.len());
 }

b2da1ecc6bd377ee90219fedb2a59323067885c8
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-25 16:11:36 +0100

    Refactor Pos and Dir
    This groups `Pos` and `Dir` struct into a `Vector2` struct.

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,20 +1,15 @@
 use anyhow::anyhow;
 
-struct Pos {
+struct Vector2 {
     pub x: usize,
     pub y: usize,
 }
 
-impl Pos {
-    pub fn add(&mut self, dir: &Dir, width: usize) {
-        self.x += dir.x % width;
+impl Vector2 {
+    pub fn add(&mut self, dir: &Vector2) {
+        self.x += dir.x;
         self.y += dir.y;
     }
-}
-
-struct Dir {
-    pub x: usize,
-    pub y: usize,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -28,15 +23,15 @@ fn main() -> anyhow::Result<()> {
         .chars()
         .count();
 
-    let dir = Dir { x: 3, y: 1 };
-    let mut pos = Pos { x: 0, y: 0 };
+    let dir = Vector2 { x: 3, y: 1 };
+    let mut pos = Vector2 { x: 0, y: 0 };
     let mut count = 0;
 
     while let Some(row) = map.get(pos.y) {
-        if let Some('#') = row.chars().nth(pos.x) {
+        if let Some('#') = row.chars().nth(pos.x % width) {
             count += 1;
         }
-        pos.add(&dir, width);
+        pos.add(&dir);
     }
 
     dbg!(count);

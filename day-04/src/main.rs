7c7453ac9612240d5380fce4a50bffffe15db43f
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-25 22:58:12 +0100

    Add parse function to validate input
    * add tests

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,4 +1,63 @@
-use std::{cmp::Ordering, collections::{HashMap, HashSet}};
+use anyhow::anyhow;
+use std::{cmp::Ordering, ops::RangeInclusive, collections::{HashMap, HashSet}, ops::Range};
+
+#[derive(Debug)]
+struct Passport {
+    pub byr: u32,
+    pub iyr: u32,
+    pub eyr: u32,
+    pub hgt: String,
+    pub hcl: String,
+    pub ecl: String,
+    pub pid: String,
+    pub cid: Option<String>,
+}
+
+pub fn parse_number(value: &str, range: RangeInclusive<u32>) -> anyhow::Result<u32> {
+    let x = value.parse::<u32>().map_err(|_| anyhow!("Failed to parse"))?;
+    if !range.contains(&x) {
+        return Err(anyhow!("Number is not in range {:?}", range));
+    }
+    Ok(x)
+}
+
+pub fn parse_byr(value: &str) -> anyhow::Result<u32> {
+    parse_number(value, 1920..=2002)
+}
+
+pub fn parse_iyr(value: &str) -> anyhow::Result<u32> {
+    parse_number(value, 2010..=2020)
+}
+
+pub fn parse_eyr(value: &str) -> anyhow::Result<u32> {
+    parse_number(value, 2020..=2030)
+}
+
+impl Passport {
+    pub fn parse(pairs: &HashMap<&str, &str>) -> anyhow::Result<Self> {
+        let byr = pairs.get("byr").ok_or_else(|| anyhow!("No byr found"))?.parse::<u32>().map_err(|_| anyhow!("Failed to parse byr"))?;
+        let iyr = pairs.get("iyr").ok_or_else(|| anyhow!("No iyr found"))?.parse::<u32>().map_err(|_| anyhow!("Failed to parse iyr"))?;
+        let eyr = pairs.get("eyr").ok_or_else(|| anyhow!("No yr found"))?.parse::<u32>().map_err(|_| anyhow!("Failed to parse eyr"))?;
+        let hgt = pairs.get("hgt").ok_or_else(|| anyhow!("No hgt found"))?;
+        let hcl = pairs.get("hcl").ok_or_else(|| anyhow!("No hcl found"))?;
+        let ecl = pairs.get("ecl").ok_or_else(|| anyhow!("No ecl found"))?;
+        let pid = pairs["byr"];
+        let cid = pairs["byr"];
+
+        Ok(
+            Self {
+                byr,
+                iyr,
+                eyr,
+                hgt: String::from(*hgt),
+                hcl: String::from(*hcl),
+                ecl: String::from(*ecl),
+                pid: String::from(pid),
+                cid: None,
+            }
+        )
+    }
+}
 
 /// Returns true if the given passport is valid
 /// It is valid when the following requirements are met
@@ -23,9 +82,8 @@ fn main() {
             passport
                 .iter()
                 .map(|line| line.split(':').collect::<Vec<_>>())
-                .filter(|pair| pair.len() == 2)
                 .map(|v| (v[0], v[1]))
-                .collect::<HashMap<_, _>>()
+                .collect()
         })
         .collect::<Vec<HashMap<&str, &str>>>();
 
@@ -36,4 +94,34 @@ fn main() {
         .collect::<Vec<_>>();
 
     dbg!(&passports.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{Passport, parse_byr};
+
+    #[test]
+    fn test_parse_valid_passport() {
+        let pairs = vec![
+            ("byr", "2002"),
+            ("iyr", "2020"),
+            ("eyr", "2022"),
+            ("hgt", "178cm"),
+            ("hcl", "#aabb99"),
+            ("ecl", "blu"),
+            ("pid", "001234567"),
+            ("cid", "147"),
+        ].into_iter().collect::<HashMap<_, _>>();
+
+        assert!(Passport::parse(&pairs).is_ok());
+    }
+
+    #[test]
+    fn test_parse_byr() {
+        assert!(parse_byr("2002").is_ok());
+        assert!(parse_byr("2003").is_err());
+        assert!(parse_byr("abcd").is_err());
+    }
 }

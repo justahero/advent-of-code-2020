5bf627a38f27f322cbbcdb339745b68007b74bda
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-26 22:17:15 +0100

    Add more grammar rules

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,17 +1,20 @@
-use anyhow::anyhow;
-
-struct BagRule {
-    pub name: String,
-    pub contents: Vec<BagRule>,
-}
 
 peg::parser!{
     grammar parser() for str {
         rule bag() -> String
             = adj:$(['a'..='z']+) " " color:$(['a'..='z']+) " bags" { format!("{} {}", adj, color) }
 
+        rule empty() -> Vec<String>
+            = "no other bags" { vec![] }
+
+        rule bags() -> Vec<String>
+            = "" { vec![] }
+
+        rule contents() -> Vec<String>
+            = empty() / bags()
+
         pub(crate) rule string() -> (String, Vec<String>)
-            = l:bag() " contain no other bags." { (l, vec![]) };
+            = l:bag() " contain " c:contents() "." { (l, vec![]) };
     }
 }
 

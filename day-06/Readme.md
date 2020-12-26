5324b5cd7f4f3594bd01fd970e79f2b44bc86076
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-26 17:18:25 +0100

    Implement logic to count group answers

--- day-06/Readme.md
+++ day-06/Readme.md
@@ -1,3 +1,55 @@
 # Day 06
 
 [Advent Of Code Day 6](https://adventofcode.com/2020/day/6).
+
+## Part 1
+
+This challenge requires a better understanding, after reading it the first it was not necessarily clear how it works. Therefore the challenge is re-iterated here as I understood it.
+
+* there are 26 yes / no question (letters of the alphabet)
+* questions whose answer is "yes" are written down one per line
+```
+abcx
+abcy
+abcz
+```
+* concludes answers of 3 people, "yes" answers for *a*, *b*, *c*, *x*, *y*, *z* weren given
+* same questions found in a group are redundant, given example means total answers of 6
+* every group is separated by a blank line
+* within each group a line contains the answers of one person
+
+For example given the following input:
+
+```
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+```
+
+There are 5 groups in total
+
+* 1 - 1 person, 3 answers
+* 2 - 3 persons, 1 answer each = 3 answers
+* 3 - 2 persons, 3 answers (*a* is redundant)
+* 4 - 4 persons, 1 answer (all answered *a*)
+* 5 - 1 person, 1 answer
+
+The task is to count all positive answers from all groups. For the given example the total amount is:
+
+```
+3 + 3 + 3 + 1 + 1 = 11
+```
+
+a total of 11.
--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,64 @@
+use itertools::Itertools;
+
+/// Counts all anwswer of this group
+fn count_answers(line: &str) -> u64 {
+    let users = line
+        .split_whitespace()
+        .map(String::from)
+        .collect::<Vec<_>>();
+
+    // combine all answers, count unique ones
+    users.join("").chars().unique().count() as u64
+}
+
+fn count_groups(groups: Vec<&str>) -> u64 {
+    groups
+        .into_iter()
+        .map(count_answers)
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // read in all the answers
+    let answers = include_str!("answers.txt")
+        .split("\n\n")
+        .collect::<Vec<_>>();
+
+    let count = count_groups(answers);
+
+    dbg!(count);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{count_answers, count_groups};
+
+    #[test]
+    fn test_count_answers() {
+        assert_eq!(3, count_answers("abc"));
+        assert_eq!(3, count_answers("a\nb\nc"));
+        assert_eq!(3, count_answers("ab\nac"));
+    }
+
+    #[test]
+    fn test_count_group_answers() {
+        assert_eq!(
+            count_groups(
+                vec![]
+            ),
+            0
+        );
+        assert_eq!(
+            count_groups(
+                vec![
+                    "abc",
+                    "a\nb\nc",
+                    "ab\nac",
+                    "a\na\na\na",
+                    "b",
+                ]
+            ),
+            11
+        );
+    }
 }

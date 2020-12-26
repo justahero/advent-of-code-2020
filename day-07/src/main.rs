c5de41919844e9c0fe94b92c10728654bafd8b55
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-26 21:29:52 +0100

    Refactor tests
    * add a parse function

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,11 +1,26 @@
+use anyhow::anyhow;
+
+struct BagRule {
+    pub name: String,
+    pub contents: Vec<BagRule>,
+}
+
+/// Parses the given rule, splits its components
+fn parse_rule(line: &str) -> anyhow::Result<()> {
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
+    use crate::parse_rule;
+
     #[test]
-    fn test_parse_rule() {
-        let rule = "light red bags contain 1 bright white bag, 2 muted yellow bags.";
+    fn test_parse_rules() {
+        assert!(parse_rule("faded blue bags contain no other bags.").is_ok());
+        assert!(parse_rule("light red bags contain 1 bright white bag, 2 muted yellow bags.").is_ok());
     }
 }

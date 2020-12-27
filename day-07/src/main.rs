291276cca37f3643f02298cfaefeba20494b17f4
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-27 19:04:41 +0100

    Add test from AoC description

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -203,4 +203,19 @@ mod tests {
         assert_eq!(5, count_bag_colors(&lines, "shiny gold").unwrap());
         assert_eq!(0, count_bag_numbers(&lines, "shiny gold").unwrap());
     }
+
+    #[test]
+    fn test_count_bag_colors_example() {
+        let lines = rules(r#"
+            shiny gold bags contain 2 dark red bags.
+            dark red bags contain 2 dark orange bags.
+            dark orange bags contain 2 dark yellow bags.
+            dark yellow bags contain 2 dark green bags.
+            dark green bags contain 2 dark blue bags.
+            dark blue bags contain 2 dark violet bags.
+            dark violet bags contain no other bags.
+        "#);
+
+        assert_eq!(126, count_bag_numbers(&lines, "shiny gold").unwrap());
+    }
 }

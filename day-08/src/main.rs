3582f4ec37d013bb800058e23b31f88c2c3256c1
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-27 22:37:56 +0100

    Add peg crate to parse commands
    This adds the [peg](https://github.com/kevinmehall/rust-peg) crate, it
    is used to parse instructions from the input text file into an enum
    `Instruction` with three possible types.

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,32 +1,87 @@
+use std::collections::HashSet;
+
+///
+#[derive(Debug, PartialEq, Eq)]
+enum Instruction {
+    Acc(i64),
+    Nop,
+    Jmp(i64),
+}
+
+peg::parser!{
+    grammar line_parser() for str {
+        rule number() -> i64
+            = s:$(['+' | '-']['0'..='9']+) { s.parse().unwrap() }
+
+        pub(crate) rule line() -> Instruction
+            = "acc " number:number() { Instruction::Acc(number) }
+            / "jmp " number:number() { Instruction::Jmp(number) }
+            / "nop " number:number() { Instruction::Nop }
+    }
+}
+
+fn parse_line(line: &str) -> anyhow::Result<Instruction> {
+    Ok(line_parser::line(line)?)
+}
+
 /// Run the given instructions
-fn run_code(instructions: &[&str]) -> anyhow::Result<i64> {
+fn run_code_part1(instructions: &[Instruction]) -> anyhow::Result<i64> {
     let mut acc = 0;
+    let mut cursor: i64 = 0;
+    let mut tape = HashSet::new();
+
+    loop {
+        match instructions.get(cursor as usize) {
+            Some(instruction) => match instruction {
+                Instruction::Acc(diff) => acc += *diff,
+                Instruction::Jmp(jmp) => cursor += *jmp,
+                _ => (),
+            },
+            None => panic!("Cursor outside instruction list")
+        };
+        if !tape.insert(cursor) || tape.len() as i64 == cursor { break; }
+        cursor += 1;
+    }
+
     Ok(acc)
 }
 
 fn main() -> anyhow::Result<()> {
     let lines = include_str!("handheld.txt")
         .lines()
+        .map(parse_line)
+        .filter_map(Result::ok)
         .collect::<Vec<_>>();
 
-    dbg!(run_code(&lines)?);
+    dbg!(run_code_part1(&lines)?);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::run_code;
+    use crate::{Instruction, parse_line, run_code_part1};
 
-    fn instructions(content: &str) -> Vec<&str> {
+    fn instructions(content: &str) -> Vec<Instruction> {
         content
             .lines()
             .map(str::trim)
+            .map(parse_line)
+            .filter_map(Result::ok)
             .collect::<Vec<_>>()
     }
 
     #[test]
-    fn test_run_code() {
+    fn test_instruction_parser() {
+        assert_eq!(Instruction::Acc(1), parse_line("acc +1").unwrap());
+        assert_eq!(Instruction::Nop, parse_line("nop +0").unwrap());
+        assert_eq!(Instruction::Jmp(-20), parse_line("jmp -20").unwrap());
+        assert_eq!(Instruction::Jmp(12), parse_line("jmp +12").unwrap());
+        assert!(parse_line("acc +n").is_err());
+    }
+
+    #[test]
+    fn test_run_code_part_1() {
         let input = instructions(r#"
             nop +0
             acc +1
@@ -39,6 +94,6 @@ mod tests {
             acc +6
         "#);
 
-        assert_eq!(5, run_code(&input).unwrap());
+        assert_eq!(5, run_code_part1(&input).unwrap());
     }
 }

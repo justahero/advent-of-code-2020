5b70e58c074c8774a586e6b8df4e6251d76caff3
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-27 21:56:42 +0100

    Add setup of day 08
    * add input file with instructions to the handheld
    * add Readme with re-iterated requirements
    * add test to check result of given example

--- day-08/Readme.md
+++ day-08/Readme.md
@@ -4,6 +4,17 @@
 
 ## Part 1
 
+Fix a handheld game console, the boot code contains an infinite loop. There are three instructions that can be executed by the program:
 
+* `nop` is a no-op instruction, nothing is executed, the next instruction below is executed
+* `acc` adds or subtracts the given number to the *accumulator*, a global register holding a value
+  * the initial value is `0`
+* `jmp` moves the current instruction cursor / counter to a new line
+  * a negative value jumps above
+  * a positive value jumps below
+
+Start processing instructions from the first line of the input.
+Execute the process until an instruction on the same line was previously run, then the infinite loop is found.
+Detect this state, once it occurs check the *accumular* value and display it.
 
 ## Part 2
--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+/// Run the given instructions
+fn run_code(instructions: &[&str]) -> anyhow::Result<i64> {
+    let mut acc = 0;
+    Ok(acc)
+}
+
+fn main() -> anyhow::Result<()> {
+    let lines = include_str!("handheld.txt")
+        .lines()
+        .collect::<Vec<_>>();
+
+    dbg!(run_code(&lines)?);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::run_code;
+
+    fn instructions(content: &str) -> Vec<&str> {
+        content
+            .lines()
+            .map(str::trim)
+            .collect::<Vec<_>>()
+    }
+
+    #[test]
+    fn test_run_code() {
+        let input = instructions(r#"
+            nop +0
+            acc +1
+            jmp +4
+            acc +3
+            jmp -3
+            acc -99
+            acc +1
+            jmp -4
+            acc +6
+        "#);
+
+        assert_eq!(5, run_code(&input).unwrap());
+    }
 }

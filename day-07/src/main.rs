c796c465fb6475d0eacaf697e3ba2ab76fc58596
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-27 14:15:18 +0100

    Refactor tests
    Extract logic to set up list of rules to its own function.

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::anyhow;
 use std::collections::HashMap;
 
-use petgraph::{graph::Graph, visit::{Bfs, Dfs}};
+use petgraph::{graph::Graph, visit::Bfs};
 
 #[derive(Debug)]
 struct Bag {
@@ -55,13 +55,13 @@ fn build_graph(bags: &[Bag]) -> anyhow::Result<Graph<Node, i32>> {
     let mut graph = Graph::<Node, i32>::new();
     let mut map = HashMap::new();
 
-    // find all nodes
+    // add all nodes
     for bag in bags {
         let value = graph.add_node(Node { color: bag.color.clone() });
         map.insert(&bag.color, value);
     }
 
-    // find all edges
+    // add all edges
     for bag in bags {
         let left = map.get(&bag.color).unwrap();
         for content in &bag.contents {
@@ -119,6 +119,13 @@ fn main() {
 mod tests {
     use crate::{count_bag_colors, parse_rule};
 
+    fn rules(content: &str) -> Vec<&str> {
+        content
+            .lines()
+            .map(|line| line.trim())
+            .collect::<Vec<_>>()
+    }
+
     #[test]
     fn test_parser_rules() {
         assert!(parse_rule("faded blue bags contain no other bags.").is_ok());
@@ -128,42 +135,32 @@ mod tests {
 
     #[test]
     fn test_count_bag_colors() {
-        const CONTENT: &str = r#"
-        light red bags contain 1 bright white bag, 2 muted yellow bags.
-        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-        bright white bags contain 1 shiny gold bag.
-        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-        faded blue bags contain no other bags.
-        dotted black bags contain no other bags.
-        "#;
-
-        let lines = CONTENT
-            .lines()
-            .map(|line| line.trim())
-            .collect::<Vec<_>>();
+        let lines = rules(r#"
+            light red bags contain 1 bright white bag, 2 muted yellow bags.
+            dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+            bright white bags contain 1 shiny gold bag.
+            muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+            shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+            dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+            vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+            faded blue bags contain no other bags.
+            dotted black bags contain no other bags.
+        "#);
 
         assert_eq!(4, count_bag_colors(&lines, "shiny gold").unwrap());
     }
 
     #[test]
     fn test_count_bag_colors_nested() {
-        const CONTENT: &str = r#"
-        light red bags contain 1 bright white bag.
-        bright white bags contain 3 dark orange bags, 4 dotted black bags.
-        dark orange bags contain 1 muted yellow bag, 2 vibrant plum bags.
-        muted yellow bags contain 2 shiny gold bags.
-        vibrant plum bags contain 1 shiny gold bags.
-        dotted black bags contain no other bags.
-        shiny gold bags contain no other bags.
-        "#;
-
-        let lines = CONTENT
-            .lines()
-            .map(|line| line.trim())
-            .collect::<Vec<_>>();
+        let lines = rules(r#"
+            light red bags contain 1 bright white bag.
+            bright white bags contain 3 dark orange bags, 4 dotted black bags.
+            dark orange bags contain 1 muted yellow bag, 2 vibrant plum bags.
+            muted yellow bags contain 2 shiny gold bags.
+            vibrant plum bags contain 1 shiny gold bags.
+            dotted black bags contain no other bags.
+            shiny gold bags contain no other bags.
+        "#);
 
         assert_eq!(5, count_bag_colors(&lines, "shiny gold").unwrap());
     }

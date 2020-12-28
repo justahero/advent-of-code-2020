d226be8861fc204c3853f81afbdc77e771f79827
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-29 00:08:34 +0100

    Add logic to create seat plan
    Parse seat plan from text file.

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,5 +1,78 @@
-fn parse_seat_plan(input: &str) -> anyhow::result<SeatPlan> {
+use std::fmt::Display;
 
+#[derive(Debug, Eq, PartialEq)]
+enum Seat {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+impl Display for Seat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match &self {
+            Seat::Floor => '.',
+            Seat::Empty => 'L',
+            Seat::Occupied => '#',
+        };
+        write!(f, "{}", s)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct SeatPlan {
+    pub width: usize,
+    pub height: usize,
+    pub seats: Vec<Seat>,
+}
+
+impl Display for SeatPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = self.seats
+            .chunks(self.width)
+            .flat_map(|row| row.iter().map(|s| format!("{}", s)).collect::<Vec<_>>())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        write!(f, "{}", s)
+    }
+}
+
+impl SeatPlan {
+    pub fn update(&self) -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            seats: vec![],
+        }
+    }
+}
+
+/// Parses the input and generates a seat plan
+fn parse_seat_plan(input: &str) -> anyhow::Result<SeatPlan> {
+    let seats = input
+        .lines()
+        .map(str::trim)
+        .collect::<Vec<_>>();
+
+    let height = seats
+        .iter()
+        .count();
+
+    let width = seats
+        .iter()
+        .map(|x| x.len())
+        .max()
+        .unwrap();
+
+    for row in seats.iter() {
+        // for 
+    }
+
+    Ok(SeatPlan {
+        width,
+        height,
+        seats: vec![],
+    })
 }
 
 fn main() {
@@ -8,9 +81,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_parse_seat_plan() {
-        let plan = r#"
+    use crate::parse_seat_plan;
+
+    const PLAN: &str = r#"
         L.LL.LL.LL
         LLLLLLL.LL
         L.L.L..L..
@@ -21,8 +94,31 @@ mod tests {
         LLLLLLLLLL
         L.LLLLLL.L
         L.LLLLL.LL
-        "#;
+    "#;
 
-        assert!(parse_seat_plan(plan).is_ok());
+    #[test]
+    fn test_parse_seat_plan() {
+        assert!(parse_seat_plan(PLAN).is_ok());
+    }
+
+    #[test]
+    fn test_update_seat_plan() {
+        let plan = parse_seat_plan(PLAN).unwrap();
+        let updated = plan.update();
+
+        let expected = parse_seat_plan(r#"
+            #.##.##.##
+            #######.##
+            #.#.#..#..
+            ####.##.##
+            #.##.##.##
+            #.#####.##
+            ..#.#.....
+            ##########
+            #.######.#
+            #.#####.##
+        "#).unwrap();
+
+        assert_eq!(expected, updated);
     }
 }

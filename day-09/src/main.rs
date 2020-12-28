827fd7c3c4eb6386ad88077aedff589f413af05d
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-28 14:49:46 +0100

    Add new function to find contiguous numbers list
    This adds a new function to find a contiguous list of numbers that add
    to the given sum.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -19,7 +19,7 @@ fn find_sums(preamble: &[u64], sum: u64) -> Vec<(u64, u64)> {
         .collect::<Vec<_>>()
 }
 
-fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<(usize, u64)> {
+fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<u64> {
     numbers
         .iter()
         .skip(preamble)
@@ -27,17 +27,36 @@ fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<(usize, u64)> {
         .find(|(index, sum)| {
             find_sums(&numbers[*index..index + preamble], **sum).is_empty()
         })
-        .map(|s| (s.0 + preamble, *s.1))
+        .map(|s| *s.1)
+}
+
+fn find_contiguous_numbers(sum: u64, numbers: &[u64]) -> Option<Vec<u64>> {
+    let result = numbers
+        .iter()
+        .batching(|iter| {
+            // do not modify current iterator
+            let copy = iter.clone();
+            None
+        })
+        .collect::<Vec<Vec<u64>>>();
+
+
+    Some(vec![])
 }
 
 fn main() {
     let numbers = parse(include_str!("numbers.txt"));
-    dbg!(find_first_number(25, &numbers));
+
+    let number = find_first_number(25, &numbers);
+    dbg!(&number);
+
+    let list = find_contiguous_numbers(number.unwrap(), &numbers);
+    dbg!(&list);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{find_first_number, find_sums};
+    use crate::{find_contiguous_numbers, find_first_number, find_sums};
 
     #[test]
     fn test_find_sums_of_pairs() {
@@ -49,6 +68,18 @@ mod tests {
         let numbers: Vec<u64> = vec![
             35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
         ];
-        assert_eq!(Some((14, 127)), find_first_number(5, &numbers));
+        assert_eq!(Some(127), find_first_number(5, &numbers));
+    }
+
+    #[test]
+    fn test_find_contiguous_numbers() {
+        let numbers: Vec<u64> = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
+        ];
+
+        assert_eq!(
+            Some(vec![15, 25, 47, 40]),
+            find_contiguous_numbers(127, &numbers),
+        );
     }
 }

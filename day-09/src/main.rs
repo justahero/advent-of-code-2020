a1fd7d827909f1a72a3ba9140338ba4015bf68a5
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-28 14:35:37 +0100

    Refactor function to return tuple
    Instead of returning the first number that is not a sum of the previous
    preamble, the index and the number are returned as a tuple. This is to
    prepare the logic for part 2 of the challenge.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -19,7 +19,7 @@ fn find_sums(preamble: &[u64], sum: u64) -> Vec<(u64, u64)> {
         .collect::<Vec<_>>()
 }
 
-fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<u64> {
+fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<(usize, u64)> {
     numbers
         .iter()
         .skip(preamble)
@@ -27,7 +27,7 @@ fn find_first_number(preamble: usize, numbers: &[u64]) -> Option<u64> {
         .find(|(index, sum)| {
             find_sums(&numbers[*index..index + preamble], **sum).is_empty()
         })
-        .map(|s| *s.1)
+        .map(|s| (s.0 + preamble, *s.1))
 }
 
 fn main() {
@@ -40,7 +40,7 @@ mod tests {
     use crate::{find_first_number, find_sums};
 
     #[test]
-    fn test_find_sums() {
+    fn test_find_sums_of_pairs() {
         assert_eq!(vec![(2, 5), (3, 4)], find_sums(&[1, 2, 3, 4, 5], 7));
     }
 
@@ -49,6 +49,6 @@ mod tests {
         let numbers: Vec<u64> = vec![
             35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
         ];
-        assert_eq!(Some(127), find_first_number(5, &numbers));
+        assert_eq!(Some((14, 127)), find_first_number(5, &numbers));
     }
 }

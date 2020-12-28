2a67422ef901698336f6adb2a8c0e60f11bb91dc
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-12-28 17:30:07 +0100

    Calculate the distribution of 1 and 3 joltages
    * add larger test from example

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -13,6 +13,13 @@ fn find_differences(adapters: &[u64]) -> Vec<u64> {
     result
 }
 
+fn find_distribution(adapters: &[u64]) -> (u64, u64) {
+    let differences = find_differences(adapters);
+    let joltage_1 = differences.iter().filter(|&x| *x == 1).count() as u64;
+    let joltage_3 = differences.iter().filter(|&x| *x == 3).count() as u64;
+    (joltage_1, joltage_3)
+}
+
 fn parse(input: &str) -> Vec<u64> {
     input
         .lines()
@@ -23,13 +30,13 @@ fn parse(input: &str) -> Vec<u64> {
 
 fn main() {
     let adapters = parse(include_str!("adapters.txt"));
-
-    println!("Hello, world!");
+    let distribution = find_distribution(&adapters);
+    dbg!(&distribution);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::find_differences;
+    use crate::{find_differences, find_distribution};
 
     #[test]
     fn test_jolt_differences() {
@@ -38,6 +45,23 @@ mod tests {
         assert_eq!(
             vec![1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3],
             find_differences(&adapters),
+        );
+        assert_eq!(
+            (7, 5),
+            find_distribution(&adapters),
+        )
+    }
+
+    #[test]
+    fn test_jolt_distribution() {
+        let adapters = vec![
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
+        ];
+
+        assert_eq!(
+            (22, 10),
+            find_distribution(&adapters),
         )
     }
 }

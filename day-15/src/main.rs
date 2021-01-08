7227112dbe4d11c1b0435208221a6902b28ee5e2
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-01-08 22:08:19 +0100

    Add function to find sequence of numbers
    * add tests with examples from the challenge

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,11 +1,21 @@
 /// Finds the sequence of numbers that are spoken.
-fn find_sequence(starter: &[u32], turns: u32) -> Vec<u32> {
-    Vec::new()
+fn find_sequence(starter: &[u64], turns: u64) -> Vec<u64> {
+    let mut sequence: Vec<u64> = starter.into();
+
+    loop {
+        if sequence.len() >= turns as usize {
+            return sequence;
+        }
+
+        let last = *sequence.last().unwrap();
+    }
 }
 
 fn main() {
     let input = vec![0, 12, 6, 13, 20, 1, 17];
-    println!("Hello, world!");
+
+    let result = find_sequence(&input, 2020);
+    dbg!(result.last());
 }
 
 #[cfg(test)]
@@ -20,5 +30,15 @@ mod tests {
         let result = find_sequence(&input, 2020);
         assert!(result.last().is_some());
         assert_eq!(436, *result.last().unwrap());
+    }
+
+    #[test]
+    fn test_find_more_sequences() {
+        assert_eq!(Some(&1), find_sequence(&[1, 3, 2], 2020).last());
+        assert_eq!(Some(&10), find_sequence(&[2, 1, 3], 2020).last());
+        assert_eq!(Some(&27), find_sequence(&[1, 2, 3], 2020).last());
+        assert_eq!(Some(&78), find_sequence(&[2, 3, 1], 2020).last());
+        assert_eq!(Some(&438), find_sequence(&[3, 2, 1], 2020).last());
+        assert_eq!(Some(&1836), find_sequence(&[3, 1, 2], 2020).last());
     }
 }

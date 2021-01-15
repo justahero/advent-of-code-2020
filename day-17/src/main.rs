5275187b41c138e0c30637e7e94f66cc76280372
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-01-16 00:10:49 +0100

    Fix iteration over dimensions
    This fixes iteration using the min and max dimensions of the grid.

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -38,7 +38,7 @@ impl Grid {
         for (y, &row) in lines.iter().enumerate() {
             for (x, c) in row.chars().enumerate() {
                 if c == '#' {
-                    cubes.push(Cube{ x: x as i32, y: y as i32, z: 0 });
+                    cubes.push(Cube{ x: x as i32, y: y as i32, z: 1 });
                 }
             }
         }
@@ -47,27 +47,33 @@ impl Grid {
     }
 
     /// Returns width of the grid
-    pub fn width(&self) -> i32 {
-        match self.cubes.iter().map(|cube| cube.x).minmax() {
-            MinMaxResult::MinMax(min, max) => max - (min - 1),
-            _ => 0,
-        }
+    pub fn min_x(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.x).min().unwrap_or(0)
     }
 
-    /// Returns height of the grid
-    pub fn height(&self) -> i32 {
-        match self.cubes.iter().map(|cube| cube.y).minmax() {
-            MinMaxResult::MinMax(min, max) => max - (min - 1),
-            _ => 0,
-        }
+    /// Returns width of the grid
+    pub fn max_x(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.x).max().unwrap_or(0)
     }
 
-    /// Returns depth of the grid
-    pub fn depth(&self) -> i32 {
-        match self.cubes.iter().map(|cube| cube.z).minmax() {
-            MinMaxResult::MinMax(min, max) => max - (min - 1),
-            _ => 0,
-        }
+    /// Returns width of the grid
+    pub fn min_y(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.y).min().unwrap_or(0)
+    }
+
+    /// Returns width of the grid
+    pub fn max_y(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.y).max().unwrap_or(0)
+    }
+
+    /// Returns width of the grid
+    pub fn min_z(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.z).min().unwrap_or(0)
+    }
+
+    /// Returns width of the grid
+    pub fn max_z(&self) -> i32 {
+        self.cubes.iter().map(|cube| cube.z).max().unwrap_or(0)
     }
 
     /// Returns the cube at 3-dimensional coordinates
@@ -79,35 +85,33 @@ impl Grid {
 
     /// Conway cycle
     pub fn cycle(grid: &Grid, num_cycles: u32) -> anyhow::Result<Grid> {
-        let mut new_grid = grid.clone();
+        let mut grid = grid.clone();
 
         for _ in 0..num_cycles {
             let mut cubes = Vec::new();
 
-            println!("CURRENT: {:?}", &grid.cubes);
-
-            for z in -1..grid.depth() + 2 {
-                for y in -1..grid.height() + 2 {
-                    for x in -1..grid.width() + 2 {
-                        let adjacent = grid.neighbors(x, y, z);
+            for x in grid.min_x()-1..grid.max_x()+2 {
+                for y in grid.min_y()-1..grid.max_y()+2 {
+                    for z in grid.min_z()-1..grid.max_z()+2 {
+                        let neighbors = grid.neighbors(x, y, z);
 
                         if grid.cube(x, y, z).is_some() {
-                            if adjacent == 2 || adjacent == 3 {
+                            if neighbors == 2 || neighbors == 3 {
                                 cubes.push(Cube{ x, y, z });
                             }
-                        } else if adjacent == 3 {
-                            cubes.push(Cube{ x, y, z });
+                        } else {
+                            if neighbors == 3 {
+                                cubes.push(Cube{ x, y, z });
+                            }
                         }
                     }
                 }
             }
 
-            println!("NEW: {:?}", &cubes);
-
-            new_grid = Grid{ cubes };
+            grid = Grid{ cubes };
         }
 
-        Ok(new_grid)
+        Ok(grid)
     }
 
     /// Returns the number of active cells
@@ -118,9 +122,9 @@ impl Grid {
     /// Returns the number of active neighbors
     pub fn neighbors(&self, x: i32, y: i32, z: i32) -> u64 {
         let mut neighbors = 0;
-        for dz in -1..=1 {
+        for dx in -1..=1 {
             for dy in -1..=1 {
-                for dx in -1..=1 {
+                for dz in -1..=1 {
                     if (dx != 0 || dy != 0 || dz != 0) && self.cube(x + dx, y + dy, z + dz).is_some() {
                         neighbors += 1;
                     }
@@ -156,9 +160,6 @@ mod tests {
         assert!(grid.is_ok());
 
         let grid = grid.unwrap();
-        assert_eq!(3, grid.width());
-        assert_eq!(3, grid.height());
-        assert_eq!(1, grid.depth());
         assert_eq!(5, grid.num_active());
         assert_eq!(5, grid.neighbors(1, 1, 0));
     }
@@ -189,6 +190,7 @@ mod tests {
 
         let grid = Grid::parse(input).unwrap();
         assert_eq!(21, Grid::cycle(&grid, 2).unwrap().num_active());
+        assert_eq!(38, Grid::cycle(&grid, 3).unwrap().num_active());
         assert_eq!(112, Grid::cycle(&grid, 6).unwrap().num_active());
     }
 }

9504ae712607830b0afc1bb6569ad7b84abf7927
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-01-15 23:19:57 +0100

    Refactor code to only hold a list of active cubes
    Instead of modeling a 3-dimensional grid, only the active cubes are kept
    in the grid. When iterating over the grid the dimensions of it are
    determined first, then the cycle can determine the next list of active
    cubes.

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,5 +1,6 @@
-use ndarray::*;
-use itertools::Itertools;
+use std::fmt::Debug;
+
+use itertools::{Itertools, MinMaxResult};
 
 /// the input grid
 const INPUT: &str = r#"
@@ -13,21 +14,16 @@ const INPUT: &str = r#"
     #.......
 "#;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum CubeState {
-    Active,
-    Inactive,
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Cube {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
 }
 
-impl Default for CubeState {
-    fn default() -> Self {
-        Self::Inactive
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Grid {
-    pub cubes: Array3::<CubeState>,
+    pub cubes: Vec<Cube>,
 }
 
 impl Grid {
@@ -38,22 +34,12 @@ impl Grid {
             .filter(|&line| !line.is_empty())
             .collect::<Vec<_>>();
 
-        let width = lines
-            .iter()
-            .map(|x| x.len())
-            .max()
-            .unwrap();
-
-        let height = lines
-            .iter()
-            .count();
-
-        let mut cubes = Array3::<CubeState>::from_elem((width, height, 1), CubeState::Inactive);
-
+        let mut cubes = Vec::new();
         for (y, &row) in lines.iter().enumerate() {
             for (x, c) in row.chars().enumerate() {
-                let state = if c == '#' { CubeState::Active } else { CubeState::Inactive };
-                cubes[[x, y, 0]] = state;
+                if c == '#' {
+                    cubes.push(Cube{ x: x as i32, y: y as i32, z: 0 });
+                }
             }
         }
 
@@ -61,107 +47,95 @@ impl Grid {
     }
 
     /// Returns width of the grid
-    pub fn width(&self) -> usize {
-        self.cubes.dim().0
+    pub fn width(&self) -> i32 {
+        match self.cubes.iter().map(|cube| cube.x).minmax() {
+            MinMaxResult::MinMax(min, max) => max - (min - 1),
+            _ => 0,
+        }
     }
 
     /// Returns height of the grid
-    pub fn height(&self) -> usize {
-        self.cubes.dim().1
+    pub fn height(&self) -> i32 {
+        match self.cubes.iter().map(|cube| cube.y).minmax() {
+            MinMaxResult::MinMax(min, max) => max - (min - 1),
+            _ => 0,
+        }
     }
-    
+
     /// Returns depth of the grid
-    pub fn depth(&self) -> usize {
-        self.cubes.dim().2
+    pub fn depth(&self) -> i32 {
+        match self.cubes.iter().map(|cube| cube.z).minmax() {
+            MinMaxResult::MinMax(min, max) => max - (min - 1),
+            _ => 0,
+        }
     }
 
     /// Returns the cube at 3-dimensional coordinates
-    pub fn cube(&self, x: i32, y: i32, z: i32) -> Option<&CubeState> {
-        if x < 0 || y < 0 || z < 0 {
-            return None;
-        }
-        self.cubes.get([x as usize, y as usize, z as usize])
+    pub fn cube(&self, x: i32, y: i32, z: i32) -> Option<&Cube> {
+        self.cubes
+            .iter()
+            .find(|&cube| cube.x == x && cube.y == y && cube.z == z)
     }
 
     /// Conway cycle
-    pub fn cycle(&self) -> anyhow::Result<Grid> {
-        let width = self.width() + 2;
-        let height = self.height() + 2;
-        let depth = self.depth() + 2;
-        let mut cubes = Array3::<CubeState>::from_elem((width, height, depth), CubeState::Inactive);
+    pub fn cycle(grid: &Grid, num_cycles: u32) -> anyhow::Result<Grid> {
+        let mut new_grid = grid.clone();
 
-        for z in 0..depth {
-            for y in 0..height {
-                for x in 0..width {
-                    let sz = z as i32;
-                    let sx = x as i32;
-                    let sy = y as i32;
+        for _ in 0..num_cycles {
+            let mut cubes = Vec::new();
 
-                    println!("({}, {}, {})", sx, sy, sz);
-                    let result = self.cube(sx, sy, sz);
-                    let state = match result {
-                        Some(state) => {
-                            let adjacent = self.neighbors(sx + 1, sy + 1, sz + 1);
-                            if *state == CubeState::Active {
-                                if adjacent == 2 || adjacent == 3 { CubeState::Active } else { CubeState::Inactive }
-                            } else if adjacent == 3 { CubeState::Active } else { CubeState::Inactive }
+            println!("CURRENT: {:?}", &grid.cubes);
+
+            for z in -1..grid.depth() + 2 {
+                for y in -1..grid.height() + 2 {
+                    for x in -1..grid.width() + 2 {
+                        let adjacent = grid.neighbors(x, y, z);
+
+                        if grid.cube(x, y, z).is_some() {
+                            if adjacent == 2 || adjacent == 3 {
+                                cubes.push(Cube{ x, y, z });
+                            }
+                        } else if adjacent == 3 {
+                            cubes.push(Cube{ x, y, z });
                         }
-                        None => CubeState::Inactive,
-                    };
-
-                    cubes[[x + 1, y + 1, z + 1]] = state;
+                    }
                 }
             }
+
+            println!("NEW: {:?}", &cubes);
+
+            new_grid = Grid{ cubes };
         }
 
-        Ok(Grid{ cubes })
+        Ok(new_grid)
     }
 
     /// Returns the number of active cells
     pub fn num_active(&self) -> usize {
-        self.cubes
-            .iter()
-            .filter(|&state| *state == CubeState::Active)
-            .count()
+        self.cubes.len()
     }
 
     /// Returns the number of active neighbors
     pub fn neighbors(&self, x: i32, y: i32, z: i32) -> u64 {
-        let list: [[i32; 3]; 3] = [
-            [-1, 0, 1],
-            [-1, 0, 1],
-            [-1, 0, 1],
-        ];
-
-        // generate all neighbors
-        let adjacent = list
-            .iter()
-            .map(IntoIterator::into_iter)
-            .multi_cartesian_product()
-            .map(|v| (*v[0] + x, *v[1] + y, *v[2] + z))
-            .collect::<Vec<_>>();
-
-        adjacent
-            .iter()
-            .filter(|(x, y, z)| *x != 0 || *y != 0 || *z != 0)
-            .map(|(x, y, z)| {
-                match self.cube(*x, *y, *z) {
-                    Some(state) => *state,
-                    None => CubeState::Inactive,
+        let mut neighbors = 0;
+        for dz in -1..=1 {
+            for dy in -1..=1 {
+                for dx in -1..=1 {
+                    if (dx != 0 || dy != 0 || dz != 0) && self.cube(x + dx, y + dy, z + dz).is_some() {
+                        neighbors += 1;
+                    }
                 }
-            })
-            .filter(|&state| state == CubeState::Active)
-            .count() as u64
+            }
+        }
+        neighbors
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut grid = Grid::parse(INPUT)?;
+    let grid = Grid::parse(INPUT)?;
 
-    for _ in 0..6 {
-        grid = grid.cycle()?;
-    }
-    dbg!(grid.num_active());
+    let result = Grid::cycle(&grid, 6)?;
+    dbg!(result.num_active());
 
     Ok(())
 }
@@ -186,6 +160,7 @@ mod tests {
         assert_eq!(3, grid.height());
         assert_eq!(1, grid.depth());
         assert_eq!(5, grid.num_active());
+        assert_eq!(5, grid.neighbors(1, 1, 0));
     }
 
     #[test]
@@ -197,27 +172,11 @@ mod tests {
         "#;
 
         let grid = Grid::parse(input).unwrap();
-        let grid = grid.cycle();
+        let grid = Grid::cycle(&grid, 1);
         assert!(grid.is_ok());
         let grid = grid.unwrap();
-        dbg!(&grid);
-        assert_eq!(5, grid.width());
-        assert_eq!(5, grid.height());
-        assert_eq!(3, grid.depth());
+
         assert_eq!(11, grid.num_active());
-    }
-
-    #[test]
-    fn test_neighbors() {
-        let input = r#"
-            .#.
-            ..#
-            ###
-        "#;
-
-        let grid = Grid::parse(input).unwrap();
-        assert_eq!(5, grid.num_active());
-        assert_eq!(5, grid.neighbors(1, 1, 0));
     }
 
     #[test]
@@ -228,6 +187,8 @@ mod tests {
             ###
         "#;
 
-        // TODO
+        let grid = Grid::parse(input).unwrap();
+        assert_eq!(21, Grid::cycle(&grid, 2).unwrap().num_active());
+        assert_eq!(112, Grid::cycle(&grid, 6).unwrap().num_active());
     }
 }

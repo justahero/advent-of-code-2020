09f607d065fe34447edc4aaf933d6978a80a4043
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-01-16 16:41:32 +0100

    Refactor parser to evaluate equation immediately
    The parser defines operator precedence with the help of this section
    https://docs.rs/peg/0.6.3/peg/#precedence-climbing . No need for an
    extra struct.

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,21 +1,37 @@
 
+peg::parser!{
+    /// Equation parser, parses the input and calculates the value of the equation
+    ///
+    /// # Example
+    /// ```
+    /// 3 + 6 + (3 * 9)
+    /// ```
+    ///
+    grammar parser() for str {
+        rule number() -> u64
+            = n:$(['0'..='9']+) { n.parse().unwrap() }
 
-struct Equation {
-}
+        rule separator() = [' ']?
 
-impl Equation {
-    pub fn solve(&self) -> u64 {
-        0
+        /// Left associated parser, multiplication & addition have same precedence
+        /// For more details see: https://docs.rs/peg/0.6.3/peg/#precedence-climbing
+        pub(crate) rule expression() -> u64
+            = precedence! {
+                x:(@) separator() "+" separator() y:@ { x + y }
+                x:(@) separator() "*" separator() y:@ { x * y }
+                "(" e:expression() ")" { e }
+                n:number() { n }
+            }
     }
 }
 
-/// Parses a line to an equation
-fn parse_equation(line: &str) -> anyhow::Result<Equation> {
-    Ok(Equation{})
+/// Parse line, evaluate equation and return result
+fn parse_equation(line: &str) -> anyhow::Result<u64> {
+    Ok(parser::expression(line)?)
 }
 
 /// Parses the string content as a list of equations
-fn parse(content: &str) -> anyhow::Result<Vec<Equation>> {
+fn parse(content: &str) -> anyhow::Result<Vec<u64>> {
     let equations = content
         .lines()
         .map(|line| line.trim())
@@ -27,8 +43,14 @@ fn parse(content: &str) -> anyhow::Result<Vec<Equation>> {
     Ok(equations)
 }
 
-fn main() {
-    let equations = parse(include_str!("equations.txt"));
+fn main() -> anyhow::Result<()> {
+    let result = parse(include_str!("equations.txt"))?
+        .iter()
+        .sum::<u64>();
+
+    dbg!(result);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -44,11 +66,11 @@ mod tests {
 
     #[test]
     fn test_solve_equations() {
-        assert_eq!(71, parse_equation("1 + 2 * 3 + 4 * 5 + 6").unwrap().solve());
-        assert_eq!(51, parse_equation("1 + (2 * 3) + (4 * (5 + 6))").unwrap().solve());
-        assert_eq!(26, parse_equation("2 * 3 + (4 * 5)").unwrap().solve());
-        assert_eq!(437, parse_equation("5 + (8 * 3 + 9 + 3 * 4 * 3)").unwrap().solve());
-        assert_eq!(12240, parse_equation("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").unwrap().solve());
-        assert_eq!(13632, parse_equation("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").unwrap().solve());
+        assert_eq!(71, parse_equation("1 + 2 * 3 + 4 * 5 + 6").unwrap());
+        assert_eq!(51, parse_equation("1 + (2 * 3) + (4 * (5 + 6))").unwrap());
+        assert_eq!(26, parse_equation("2 * 3 + (4 * 5)").unwrap());
+        assert_eq!(437, parse_equation("5 + (8 * 3 + 9 + 3 * 4 * 3)").unwrap());
+        assert_eq!(12240, parse_equation("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").unwrap());
+        assert_eq!(13632, parse_equation("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").unwrap());
     }
 }

549161b816891c9bb757a28dc4c25f68afd2312d
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-01-17 17:07:33 +0100

    Refactor code to pass depth
    * add test for part2 rules change

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -73,7 +73,7 @@ fn validate(rules: &HashMap<u64, Rule>, messages: &[String]) -> u64 {
     messages
         .iter()
         .filter(|&message| {
-            match_rule(message.as_bytes(), rules, 0)
+            match_rule(message.as_bytes(), rules, 0, 0)
                 .map(|n| n == message.len())
                 .unwrap_or(false)
         })
@@ -81,7 +81,7 @@ fn validate(rules: &HashMap<u64, Rule>, messages: &[String]) -> u64 {
 }
 
 /// Tries to apply the rule to the given message
-fn match_rule(message: &[u8], rules: &HashMap<u64, Rule>, rule: u64) -> Option<usize> {
+fn match_rule(message: &[u8], rules: &HashMap<u64, Rule>, rule: u64, depth: u32) -> Option<usize> {
     if message.is_empty() {
         return None;
     }
@@ -92,21 +92,26 @@ fn match_rule(message: &[u8], rules: &HashMap<u64, Rule>, rule: u64) -> Option<u
         Rule::List(numbers) => {
             numbers
                 .iter()
-                .try_fold(0, |count, &r| match_rule(&message[count..], rules, r).map(|n| n + count))
+                .try_fold(0, |count, &r| match_rule(&message[count..], rules, r, depth + 1).map(|n| n + count))
         }
         Rule::Tuples((lhs, rhs)) => {
             lhs.iter()
-                .try_fold(0, |count, &r| match_rule(&message[count..], rules, r).map(|n| n + count))
+                .try_fold(0, |count, &r| match_rule(&message[count..], rules, r, depth + 1).map(|n| n + count))
                 .or_else(|| {
                     rhs.iter()
-                        .try_fold(0, |count, &r| match_rule(&message[count..], rules, r).map(|n| n + count))
+                        .try_fold(0, |count, &r| match_rule(&message[count..], rules, r, depth + 1).map(|n| n + count))
                 })
         }
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let (rules, messages) = parse(include_str!("messages.txt"))?;
+    let (mut rules, messages) = parse(include_str!("messages.txt"))?;
+    let result = validate(&rules, &messages);
+    dbg!(result);
+
+    rules.insert(8, Rule::Tuples((vec![42], vec![42, 8])));
+    rules.insert(11, Rule::Tuples((vec![42, 31], vec![42, 11, 31])));
     let result = validate(&rules, &messages);
     dbg!(result);
 
@@ -115,7 +120,7 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, validate};
+    use crate::{Rule, parse, validate};
 
     const CONTENT: &str = r#"
         0: 4 1 5
@@ -146,5 +151,65 @@ mod tests {
     fn test_validate_messages() {
         let (rules, messages) = parse(CONTENT).unwrap();
         assert_eq!(2, validate(&rules, &messages));
+    }
+
+    #[test]
+    fn test_validate_part2_messages() {
+        let content = r#"
+            42: 9 14 | 10 1
+            9: 14 27 | 1 26
+            10: 23 14 | 28 1
+            1: "a"
+            11: 42 31
+            5: 1 14 | 15 1
+            19: 14 1 | 14 14
+            12: 24 14 | 19 1
+            16: 15 1 | 14 14
+            31: 14 17 | 1 13
+            6: 14 14 | 1 14
+            2: 1 24 | 14 4
+            0: 8 11
+            13: 14 3 | 1 12
+            15: 1 | 14
+            17: 14 2 | 1 7
+            23: 25 1 | 22 14
+            28: 16 1
+            4: 1 1
+            20: 14 14 | 1 15
+            3: 5 14 | 16 1
+            27: 1 6 | 14 18
+            14: "b"
+            21: 14 1 | 1 14
+            25: 1 1 | 1 14
+            22: 14 14
+            8: 42
+            26: 14 22 | 1 20
+            18: 15 15
+            7: 14 5 | 1 21
+            24: 14 1
+
+            abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+            bbabbbbaabaabba
+            babbbbaabbbbbabbbbbbaabaaabaaa
+            aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+            bbbbbbbaaaabbbbaaabbabaaa
+            bbbababbbbaaaaaaaabbababaaababaabab
+            ababaaaaaabaaab
+            ababaaaaabbbaba
+            baabbaaaabbaaaababbaababb
+            abbbbabbbbaaaababbbbbbaaaababb
+            aaaaabbaabaaaaababaa
+            aaaabbaaaabbaaa
+            aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+            babaaabbbaaabaababbaabababaaab
+            aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+        "#;
+
+        let (mut rules, messages) = parse(content).unwrap();
+        assert_eq!(3, validate(&rules, &messages));
+
+        rules.insert(8, Rule::Tuples((vec![42], vec![42, 8])));
+        rules.insert(11, Rule::Tuples((vec![42, 31], vec![42, 11, 31])));
+        assert_eq!(12, validate(&rules, &messages));
     }
 }

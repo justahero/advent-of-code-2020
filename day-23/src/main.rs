d38f1b0aa848d0b0ebcb7ed18265f7101c1d99e3
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-03-01 23:55:17 +0100

    Output ordered numbers

--- day-23/src/main.rs
+++ day-23/src/main.rs
@@ -7,6 +7,8 @@
 /// * re-arrange the result list
 /// * adjust the initial cycle with index offset
 ///
+/// TODO refactor for part 2 !!! :(
+///
 fn next_move(current_index: usize, circle: &[u64]) -> Vec<u64> {
     let mut list = circle
         .iter()
@@ -60,16 +62,32 @@ fn play_game(num_rounds: usize, circle: &[u64]) -> Vec<u64> {
         })
 }
 
+/// Creates the ordered string
+fn ordered_number(numbers: &[u64]) -> String {
+    let index = numbers.iter().position(|&item| item == 1).unwrap();
+
+    numbers
+        .iter()
+        .cycle()
+        .skip(index + 1)
+        .take(numbers.len() - 1)
+        .map(|val| val.to_string())
+        .collect::<Vec<_>>()
+        .join("")
+}
+
 fn main() {
     let cups = vec![3, 9, 4, 6, 1, 8, 5, 2, 7];
     let result = play_game(100, &cups);
+    dbg!(&result);
 
-    dbg!(result);
+    let order = ordered_number(&result);
+    dbg!(order);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{next_move, play_game};
+    use crate::{next_move, ordered_number, play_game};
 
     #[test]
     fn test_next_move() {
@@ -85,5 +103,13 @@ mod tests {
         assert_eq!(vec![7, 2, 5, 8, 9, 1, 3, 4, 6], play_game(3, &cups));
         assert_eq!(vec![3, 2, 5, 8, 4, 6, 7, 9, 1], play_game(4, &cups));
         assert_eq!(vec![9, 2, 5, 8, 4, 1, 3, 6, 7], play_game(5, &cups));
+        assert_eq!(vec![5, 8, 3, 7, 4, 1, 9, 2, 6], play_game(10, &cups));
+    }
+
+    #[test]
+    fn test_ordered_number() {
+        let cups = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];
+        let result = play_game(100, &cups);
+        assert_eq!("67384529".to_string(), ordered_number(&result));
     }
 }

12da26c53cafde5938ebfcd4d2e730c5a944f164
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-03-10 22:35:36 +0100

    Add move function to Tile
    * add tests

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -30,6 +30,7 @@ struct Tile {
 }
 
 impl Tile {
+    /// Parse the tile with the given list of directions
     pub fn parse(line: &str) -> anyhow::Result<Self> {
         let directions = parser::tile(line)?;
 
@@ -37,11 +38,39 @@ impl Tile {
             directions,
         })
     }
+
+    /// Move along directions and return final tile position as tuple (x,y)
+    /// Movement is based on this article: https://www.redblobgames.com/grids/hexagons/
+    /// (odd-r horizontal layout shoves odd rows right)
+    /// The reference tile is located at (0, 0)
+    ///
+    pub fn last_tile(&self) -> (i32, i32) {
+        self.directions
+            .iter()
+            .fold((0, 0), |pos, dir| {
+                match dir {
+                    Dir::E => (pos.0 + 1, pos.1),
+                    Dir::SE => (pos.0, pos.1 + 1),
+                    Dir::SW => (pos.0 - 1, pos.1 + 1),
+                    Dir::W => (pos.0 - 1, pos.1),
+                    Dir::NW => (pos.0, pos.1 - 1),
+                    Dir::NE => (pos.0 + 1, pos.1 - 1),
+                }
+            })
+    }
 }
 
 /// Parse the list of tiles / directions
 fn parse_tiles(content: &str) -> anyhow::Result<Vec<Tile>> {
-    Ok(vec![])
+    let tiles = content
+        .lines()
+        .map(str::trim)
+        .filter(|&line| !line.is_empty())
+        .map(|line| Tile::parse(line))
+        .filter_map(Result::ok)
+        .collect();
+
+    Ok(tiles)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -52,15 +81,55 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Dir, Tile};
+    use crate::{Dir, Tile, parse_tiles};
+
+    const TILES: &str = r#"
+        sesenwnenenewseeswwswswwnenewsewsw
+        neeenesenwnwwswnenewnwwsewnenwseswesw
+        seswneswswsenwwnwse
+        nwnwneseeswswnenewneswwnewseswneseene
+        swweswneswnenwsewnwneneseenw
+        eesenwseswswnenwswnwnwsewwnwsene
+        sewnenenenesenwsewnenwwwse
+        wenwwweseeeweswwwnwwe
+        wsweesenenewnwwnwsenewsenwwsesesenwne
+        neeswseenwwswnwswswnw
+        nenwswwsewswnenenewsenwsenwnesesenew
+        enewnwewneswsewnwswenweswnenwsenwsw
+        sweneswneswneneenwnewenewwneswswnese
+        swwesenesewenwneswnwwneseswwne
+        enesenwswwswneneswsenwnewswseenwsese
+        wnwnesenesenenwwnenwsewesewsesesew
+        nenewswnwewswnenesenwnesewesw
+        eneswnwswnwsenenwnwnwwseeswneewsenese
+        neswnwewnwnwseenwseesewsenwsweewe
+        wseweeenwnesenwwwswnew
+    "#;
 
     #[test]
     fn test_parse_tile() {
-        let tile = Tile::parse("esenee").unwrap();
-
         assert_eq!(
             vec![Dir::E, Dir::SE, Dir::NE, Dir::E],
-            tile.directions,
-        )
+            Tile::parse("esenee").unwrap().directions,
+        );
+
+        assert_eq!(
+            vec![Dir::NW, Dir::W, Dir::SW, Dir::E, Dir::E],
+            Tile::parse("nwwswee").unwrap().directions,
+        );
+    }
+
+    #[test]
+    fn test_parse_tiles() {
+        let tiles = parse_tiles(TILES);
+
+        assert!(tiles.is_ok());
+        assert_eq!(20, tiles.unwrap().len());
+    }
+
+    #[test]
+    fn test_last_tiles() {
+        assert_eq!((3, 0), Tile::parse("esenee").unwrap().last_tile());
+        assert_eq!((0, 0), Tile::parse("nwwswee").unwrap().last_tile());
     }
 }
